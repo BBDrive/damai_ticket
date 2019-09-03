f084a85b446353bbcbf43af9ef9d72c9ebba9550
BBDrive <842089432@qq.com>
2019-09-03 17:29:03 +0800

    大麦抢票工具

---
+++ README.md
@@ -0,0 +1,62 @@
+# damai_ticket
+大麦网自动抢票工具
+
+## Preliminary
+Python 3.6+
+
+## Set up
+Firefox Browser (测试版本：v68.0.1.7137)
+
+geckodriver.exe (测试版本：v0.24.0)
+
+--以上文件夹内包含--
+
+pip install selenium
+
+## Basic usage
+在config.json中输入相应配置信息，具体说明如下：
+
+{
+    
+    "sess": [ # 场次优先级列表，如本例中共有三个场次，根据下表，则优先选择1，再选择2，最后选择3；也可以仅设置1个。
+        1,
+        2,
+        3,
+    ],
+    "price": [ # 票价优先级，如本例中共有三档票价，根据下表，则优先选择1，再选择3；也可以仅设置1个。
+        1,
+        3
+    ],
+    "real_name": [1,2], # 实名者序号，如本例中根据序号共选择两位实名者，根据序号，也可仅选择一位
+                        （选择一位或是多位根据购票需知要求，若一个订单仅需提供一位购票人信息则选择一位；
+                        若一张门票对应一位购票人信息则选择多位）。
+    "nick_name": "<Your nick_name>", # <Your nick_name>改为用户的昵称，用于验证登录是否成功
+    "ticket_num": 2, # 购买票数
+    "damai_url": "https://www.damai.cn/", # 大麦网官网网址
+    "target_url": "https://detail.damai.cn/item.htm?id=599834886497" # 目标购票网址
+    
+}
+
+1）部分门票需要选择城市，只需选择相应城市后将其网址复制到config.json文件的target_url参数即可。
+
+![avatar](/picture/1.png)
+
+2）根据需要选择的场次和票价分别修改config.json文件中的sess和price参数。
+
+![avatar](/picture/2.png)
+
+3）查看购票须知中实名制一栏，若每笔订单只需一个证件号则config.json文件中的real_name参数只需选择一个；若每张门票需要一个证件号，则real_name参数根据需购票数量进行相应添加。
+
+![avatar](/picture/3.png)
+
+若是首次登录，根据终端输出的提示，依次点击登录、扫码登录，代码将自动保存cookie文件（cookie.pkl）
+
+使用前请将待抢票者的姓名、手机、地址设为默认。
+
+配置完成后执行python Autoticket.py即可。
+
+本代码为保证抢票顺利，设置循环直到抢票成功才退出循环，若中途需要退出程序请直接终止程序。
+
+## Ref
+本代码基于以下的Repo进行简单的改进：
+Entromorgan:https://github.com/Entromorgan/Autoticket
---
+++ damai_ticket.py
@@ -0,0 +1,363 @@
+# coding: utf-8
+from json import loads
+from time import sleep, time
+from pickle import dump, load
+from os.path import exists
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Concert(object):
+    def __init__(self, session, price, real_name, nick_name, ticket_num, damai_url, target_url):
+        self.session = session # 场次序号优先级
+        self.price = price # 票价序号优先级
+        self.real_name = real_name # 实名者序号
+        self.status = 0 # 状态标记
+        self.time_start = 0 # 开始时间
+        self.time_end = 0 # 结束时间
+        self.num = 0 # 尝试次数
+        self.type = 0 # 目标购票网址类别
+        self.ticket_num = ticket_num # 购买票数
+        self.nick_name = nick_name # 用户昵称
+        self.damai_url = damai_url # 大麦网官网网址 
+        self.target_url = target_url # 目标购票网址
+
+        if self.target_url.find("detail.damai.cn") != -1:
+            self.type = 1
+        elif self.target_url.find("piao.damai.cn") != -1:
+            self.type = 2
+        else:
+            self.type = 0
+            raise Exception("***Error:Unsupported Target Url Format:{}***".format(self.target_url))
+            self.driver.quit()
+    
+    
+    def isClassPresent(self, item, name, ret=False):
+        try:
+            result = item.find_element_by_class_name(name)
+            if ret:
+                return result
+            else:
+                return True
+        except:
+            return False
+            
+            
+    def get_cookie(self):
+        self.driver.get(self.damai_url)
+        print("###请点击登录###")
+        while self.driver.title.find('大麦网-全球演出赛事官方购票平台')!=-1: # 等待网页加载完成
+            sleep(1)
+        print("###请扫码登录###")
+        while self.driver.title == '大麦登录': # 等待扫码完成
+            sleep(1)
+        dump(self.driver.get_cookies(), open("cookies.pkl", "wb")) 
+        print("###Cookie保存成功###")
+    
+    
+    def set_cookie(self):
+        try:
+            cookies = load(open("cookies.pkl", "rb")) # 载入cookie
+            for cookie in cookies:
+                cookie_dict = {
+                    'domain':'.damai.cn', # 必须有，不然就是假登录
+                    'name': cookie.get('name'),
+                    'value': cookie.get('value'),
+                    "expires": "",
+                    'path': '/',
+                    'httpOnly': False,
+                    'HostOnly': False,
+                    'Secure': False}
+                self.driver.add_cookie(cookie_dict)
+            print('###载入Cookie###')
+        except Exception as e:
+            print(e)
+            
+            
+    def login(self):
+        print('###开始登录###')
+        if not exists('cookies.pkl'):# 如果不存在cookie.pkl,就获取一下
+            self.get_cookie()
+        self.driver.get(self.target_url)
+        self.set_cookie()
+     
+    
+    def enter_concert(self):
+        print('###打开浏览器，进入大麦网###')
+        self.driver = webdriver.Firefox() # 默认火狐浏览器
+        self.driver.maximize_window()
+        self.login()
+        self.driver.refresh()
+        try:
+            if self.type == 1: # detail.damai.cn
+                locator = (By.XPATH, "/html/body/div[1]/div/div[3]/div[1]/a[2]/div")
+            elif self.type == 2: # piao.damai.cn
+                locator = (By.XPATH, "/html/body/div[1]/div/ul/li[2]/div/label/a[2]")
+            element = WebDriverWait(self.driver, 3, 0.3).until(EC.text_to_be_present_in_element(locator,self.nick_name))
+            self.status = 1
+            print("###登录成功###")
+        except:
+            self.status=0
+            raise Exception("***错误：登录失败,请删除cookie后重试***") 
+            self.driver.quit()
+    
+    
+    def choose_ticket_1(self): # for type 1, i.e., detail.damai.cn           
+        self.time_start = time() 
+        print("###开始进行日期及票价选择###")
+        
+        while self.driver.title.find('确认订单') == -1:  # 如果跳转到了确认界面就算这步成功了，否则继续执行此步
+            self.num += 1
+
+            try:
+                box = self.driver.find_element_by_class_name('perform__order__box')
+                # print('可选区域数量为：{}'.format(len(selects)))
+                buybutton = box.find_element_by_class_name('buybtn')
+                buybutton_text = buybutton.text
+            except:
+                print("***Error: box,selects or buybutton 位置找不到***")
+                continue
+
+            if buybutton_text == "即将开抢" or buybutton_text == "即将开售":
+                self.status = 2
+                self.driver.refresh()
+                print('---尚未开售，刷新等待---')
+                continue
+
+            try:
+                selects = box.find_elements_by_class_name('perform__order__select')
+                for item in selects:
+                    if item.find_element_by_class_name('select_left').text == '场次':
+                        session = item
+                        # print('\t场次定位成功')
+                    elif item.find_element_by_class_name('select_left').text == '票档':
+                        price = item
+                        # print('\t票档定位成功')
+
+                session_list = session.find_elements_by_class_name('select_right_list_item')
+                # print('可选场次数量为：{}'.format(len(session_list)))
+                for i in self.session: # 根据优先级选择一个可行场次
+                    j = session_list[i-1]
+                    k = self.isClassPresent(j, 'presell', True)
+                    if k: # 如果找到了带presell的类
+                        if k.text == '无票':
+                            continue
+                        elif k.text == '预售':
+                            j.click()
+                            break
+                    else:
+                        j.click()
+                        break
+
+                price_list = price.find_elements_by_class_name('select_right_list_item')
+                # print('可选票档数量为：{}'.format(len(price_list)))
+                for i in self.price:
+                    j = price_list[i-1]
+                    k = self.isClassPresent(j, 'notticket')
+                    if k: # 存在notticket代表存在缺货登记，跳过
+                        continue
+                    else:
+                        j.click()
+                        break
+            except:
+                print("***Error: 选择场次or票档不成功***")
+                continue
+
+            try:
+                ticket_num_up = box.find_element_by_class_name('cafe-c-input-number-handler-up')
+            except:
+                print("***Error: ticket_num_up 位置找不到***")
+                continue
+
+            if buybutton_text == "立即预订":
+                for i in range(self.ticket_num-1): # 设置增加票数
+                    ticket_num_up.click()
+                buybutton.click()
+                self.status = 3
+
+            elif buybutton_text == "立即购买":
+                for i in range(self.ticket_num-1): # 设置增加票数
+                    ticket_num_up.click()
+                buybutton.click()
+                self.status = 4
+
+            elif buybutton_text == "选座购买": # 选座购买暂时无法完成自动化
+                buybutton.click()
+                self.status = 5
+                print("###请自行选择位置和票价###") # 此处或可改成input，等待用户选完后反馈，继续抢票流程
+                break
+
+            elif buybutton_text == "提交缺货登记":
+                print('###抢票失败，请手动提交缺货登记###')
+                break
+            
+            
+    def choose_ticket_2(self): # for type 2, i.e., piao.damai.cn
+        self.time_start = time() 
+        print("###开始进行日期及票价选择###")
+        
+        while self.driver.title.find('订单结算页') == -1:  # 如果跳转到了确认界面就算这步成功了，否则继续执行此步
+            self.num += 1
+            session = self.driver.find_element_by_id('performList')
+            session_list = session.find_elements_by_tag_name('li')
+            print('可选场次数量为：{}'.format(len(session_list)))
+            for i in self.session: # 根据优先级选择一个可行场次,目前暂时没有找到有不可行日期的案例
+                j = session_list[i-1].get_attribute('class')
+                if j == 'itm': # 未选中
+                    price_list[i-1].find_element_by_tag_name('a').click()
+                    break
+                elif j == 'itm itm-sel': # 已选中
+                     break
+                elif j == 'itm itm-oos': # 无法选中
+                    continue 
+            sleep(0.7)
+            price = self.driver.find_element_by_id('priceList')
+            price_list = price.find_elements_by_tag_name('li')
+            # print('可选票档数量为：{}'.format(len(price_list)))
+            for i in self.price:
+                j = price_list[i-1].get_attribute('class')
+                if j == 'itm': # 未选中
+                    price_list[i-1].find_element_by_tag_name('a').click()
+                    break
+                elif j == 'itm itm-sel': # 已选中
+                     break
+                elif j == 'itm itm-oos': # 无法选中
+                    continue 
+                    
+            # 需要先判断是否存在按钮，才能确定是否会出现添加票
+            sleep(0.5)
+            try:
+                buybutton = self.driver.find_element_by_id('btnBuyNow') # 要改成立即预订按钮的id
+                self.status = 3
+            except:
+                try:
+                    buybutton = self.driver.find_element_by_id('btnBuyNow')
+                    self.status = 4
+                except:
+                    print('###无法立即购买，尝试自行选座###')
+                    try:
+                        buybutton = self.driver.find_element_by_id('btnXuanzuo')
+                        self.status = 5
+                        print("###请自行选择位置和票价###") # 此处或可改成input，等待用户选完后反馈，继续抢票流程
+                        break
+                    except:
+                        print('---尚未开售，刷新等待---')
+                        self.status = 2
+                        self.driver.refresh()
+            if self.ticket_num > 1:# 自动添加购票数
+                add = self.driver.find_element_by_class_name('btn-add')
+                while add.is_displayed() != True: # 等待显示
+                    continue
+                for i in range(self.ticket_num-1):
+                    add.click()
+            buybutton.click()
+            # 目前没有找到缺货没有按钮的情况
+            
+    
+    def check_order_1(self):
+        if self.status in [3, 4, 5]:
+            print('###开始确认订单###')
+            print('###选择购票人信息###')
+            try:
+                tb = WebDriverWait(self.driver, 10, 0.2).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div/div[2]/div[2]/div[1]')))
+                init_sleeptime = 0.
+                Labels = tb.find_elements_by_tag_name('label')
+                for num_people in self.real_name:
+                    Labels[num_people-1].find_element_by_tag_name('input').click() # 选择第self.real_name个实名者
+                    if num_people != self.real_name[-1]:
+                        sleep(init_sleeptime)
+
+                # 防止点击过快导致没有选择多个人
+                all_true = False
+                while not all_true:
+                    init_sleeptime += 0.1
+                    true_num = 0
+                    for num_people in self.real_name:
+                        tag_input = Labels[num_people-1].find_element_by_tag_name('input')
+                        if tag_input.get_attribute('aria-checked') == 'false':
+                            sleep(init_sleeptime)
+                            tag_input.click()
+                        else:
+                            true_num += 1
+                    if true_num == len(self.real_name):
+                        break
+
+            except:
+                raise Exception("***Error：实名信息选择框没有显示***")
+            # print('###不选择订单优惠###')
+            # print('###请在付款完成后下载大麦APP进入订单详情页申请开具###')
+            self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[9]/button').click() # 同意以上协议并提交订单
+            '''
+            try:
+                buttons = self.driver.find_elements_by_tag_name('button') # 找出所有该页面的button
+                for button in buttons:
+                    if button.text == '同意以上协议并提交订单':
+                        button.click()
+                        break
+            except Exception as e:
+                raise Exception('***错误：没有找到提交订单按钮***')
+           '''
+            try:
+                element = WebDriverWait(self.driver, 10, 0.2).until(EC.title_contains('支付宝'))
+                self.status = 6
+                print('###成功提交订单,请手动支付###')
+                self.time_end = time()
+            except:
+                raise Exception('***Error: 提交订单失败***')
+                
+                
+    def check_order_2(self):
+        if self.status in [3,4,5]:
+            print('###开始确认订单###')
+            print('###选择购票人信息###')   
+            try:
+                tb = WebDriverWait(self.driver, 3, 0.3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[3]/div[3]/div[2]/div[2]/div/div/div/h2/a')))
+                tb.click()
+                sleep(0.3)
+                lb_list = self.driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[12]/div/div[2]/div/div[2]/div/table/tbody')
+                lb_list.find_elements_by_tag_name('input')[self.real_name-1].click() # 选择第self.real_name个实名者
+            except Exception as e:
+                print("###实名信息选择框没有显示###")
+            # print('###不选择订单优惠###')
+            # print('###请在付款完成后下载大麦APP进入订单详情页申请开具###')
+            self.driver.find_element_by_id('orderConfirmSubmit').click() # 同意以上协议并提交订单
+            element = WebDriverWait(self.driver, 10, 2).until(EC.title_contains('选择支付方式'))
+            element.find_element_by_xpath('/html/body/div[5]/div/div/div/ul/li[2]/a').click() # 默认选择支付宝
+            element.find_element_by_xpath('/html/body/div[5]/div/div/form/div[2]/ul/li[1]/label/input').click()
+            element.find_element_by_id('submit2').click() # 确认无误，支付
+            self.status = 6
+            print('###成功提交订单,请手动支付###')
+            self.time_end = time()
+            # print('###提交订单失败,请查看问题###') # 这里异常处理还有点问题
+                        
+                    
+    def finish(self):
+        if self.status == 6: # 说明抢票成功
+            print("###经过%d轮奋斗，共耗时%f秒，抢票成功！请确认订单信息###"%(self.num, round(self.time_end-self.time_start,3)))
+
+
+if __name__ == '__main__':
+    try:
+        with open('./config.json', 'r', encoding='utf-8') as f:
+            config = loads(f.read())
+            # params: 场次优先级，票价优先级，实名者序号, 用户昵称， 购买票数， 官网网址， 目标网址
+        con = Concert(config['sess'], config['price'], config['real_name'], config['nick_name'], config['ticket_num'], config['damai_url'], config['target_url'])
+        con.enter_concert()
+    except Exception as e:
+        print(e)
+    while True:
+        try:
+            if con.type == 1: # detail.damai.cn
+                con.choose_ticket_1()
+                con.check_order_1()
+            elif con.type == 2: # piao.damai.cn
+                con.choose_ticket_2()
+                con.check_order_2()
+            if con.status == 6:
+                con.finish()
+                break
+        except Exception as e:
+            print(e)
+            continue
